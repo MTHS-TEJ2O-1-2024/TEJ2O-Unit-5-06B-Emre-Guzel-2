4cdbb65930779f67939134407bdb194aa17efbc8
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-16 11:26:44 -0400

    added the distqance

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 16 2024
+ * This program measures the distance  
 */
 
-basic.showString('Hello, World!')
+let  distanceToObject:number = 0
+
+// Setup
+basic.showIcon(IconNames.Happy)
+
+// Find the distance 
+input.onButtonPressed(Button.A,function(){
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+       DigitalPin.P1,
+       DigitalPin.P2,
+       PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
