94c2d84979c103fa9166cea9f3aa56b9f44eefd7
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-18 00:21:09 +0000

    added sonar

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,13 +5,63 @@ This module is a Micro:bit MicroPython program claculates the distance
 """
 
 from microbit import *
-#class HCSR04:
-sonar = HCSR04()
 
+
+class HCSR04:
+    # this class abstracts out the functionality of the HC-SR04 and
+    #   returns distance in mm
+    # Trig: pin 1
+    # Echo: pin 2
+    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
+        self.trigger_pin = tpin
+        self.echo_pin = epin
+        self.sclk_pin = spin
+
+    def distance_mm(self):
+        spi.init(
+            baudrate=125000,
+            sclk=self.sclk_pin,
+            mosi=self.trigger_pin,
+            miso=self.echo_pin,
+        )
+        pre = 0
+        post = 0
+        k = -1
+        length = 500
+        resp = bytearray(length)
+        resp[0] = 0xFF
+        spi.write_readinto(resp, resp)
+        # find first non zero value
+        try:
+            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
+        except StopIteration:
+            i = -1
+        if i > 0:
+            pre = bin(value).count("1")
+            # find first non full high value afterwards
+            try:
+                k, value = next(
+                    (ind, v)
+                    for ind, v in enumerate(resp[i : length - 2])
+                    if resp[i + ind + 1] == 0
+                )
+                post = bin(value).count("1") if k else 0
+                k = k + i
+            except StopIteration:
+                i = -1
+        dist = -1 if i < 0 else round(((pre + (k - i) * 8.0 + post) * 8 * 0.172) / 2)
+        return dist
+
+
+sonar = HCSR04()
 # Dfeinding the distance
-distance_TO_Object = 0
+distance_TO_Object = 1
 display.show(Image.HAPPY)
 
-if button_a.is_pressed():
-    display.clear()
-    distance_TO_Object = sonar.ping
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        distance_TO_Object = sonar.distance_mm()
+        #to find cm value, you need to divide mm value to 10
+        display.show(int(sonar.distance_mm()/10))
+        sleep(500)
